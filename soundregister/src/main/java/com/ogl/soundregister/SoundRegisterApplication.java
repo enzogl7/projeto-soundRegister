eed9636063397c70d815045a1a1bd886913bcfab
Enzo Lima <enzolima527@gmail.com>
2024-11-25 10:15:39 -0300

    .

---
+++ soundregister/src/main/java/com/ogl/soundregister/SoundRegisterApplication.java
@@ -0,0 +1,13 @@
+package com.ogl.soundregister;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoundRegisterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoundRegisterApplication.class, args);
+	}
+
+}
---
+++ soundregister/src/test/java/com/ogl/soundregister/SoundRegisterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ogl.soundregister;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoundRegisterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
